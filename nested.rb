f0d43aaac7d17bc4d1768cc0b32e07dc08c10f5e
Devone Austin <devone.austin@gmail.com>
2020-04-19 18:49:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -91,13 +91,13 @@ def adding_matz
 		    									:languages => ["LISP", "C"]},
 		 										:grace_hopper => {
 		 											:known_for => "COBOL",
-		 											:languages => ["COBOL", "FORTRAN"]},											
+		 											:languages => ["COBOL", "FORTRAN"]},
 												:alan_kay => {
 													:known_for => "Object Orientation",
 							          	:languages => ["Smalltalk", "LISP"]},
 												:dennis_ritchie => {
 														:known_for => "Unix",
-														:languages => ["C"]}												
+														:languages => ["C"]}
 		 programmer_hash[:yukihiro_matsumoto]
 
 end
