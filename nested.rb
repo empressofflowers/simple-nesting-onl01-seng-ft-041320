fcf0a84f09811ff4a9e999cae5a6c1506494cb0d
Devone Austin <devone.austin@gmail.com>
2020-04-19 19:06:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,9 +87,8 @@ def adding_matz
         }
      }
 
-		 programmer_hash[:yukihiro_matsumoto][:grace_hopper][:alan_kay][:dennis_ritchie]
 		  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-		 programmer_hash
+		 programmer_hash[:yukihiro_matsumoto]
 
 end
 
